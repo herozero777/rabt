c13e44ef8c970c3e001a73b6f2d247f0c51f0003
herozero777 <ismailhero@gmail.com>
2017-01-12 15:31:22 +0500

    com 2

---
+++ rabtSpring/src/main/java/rabt/model/Profile.java
@@ -0,0 +1,39 @@
+package rabt.model;
+
+import java.util.HashSet;
+
+public class Profile {
+	private User u_id;
+	private String gender;
+	private String email;
+	private String school;
+	private String aboutme;
+	//private Set<Tags> tags = new HashSet<>(0);
+	private Set<Tags> tags;
+	
+	public int getU_id() { return u_id; }
+	public void setU_id(int u_id) {
+		this.u_id = u_id;
+	}
+	
+	public String getGender() { return gender; }
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public String getEmail() { return email; }
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSchool() { return school; }
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+	public String getAboutme() { return aboutme; }
+	public void setAboutme(String aboutme) {
+		this.aboutme = aboutme;
+	}
+	
+}
---
+++ rabtSpring/src/main/java/rabt/model/Tags.java
@@ -0,0 +1,11 @@
+package rabt.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Tags {
+	private int id;
+	private String name;
+	//private Set<Profile> profile = new HashSet<>(0);
+	private Set<Profile> profile;
+}
