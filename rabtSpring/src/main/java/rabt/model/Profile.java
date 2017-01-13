66e470bff7a5d9241b789cf0cc626daa39489d7e
herozero777 <ismailhero@gmail.com>
2017-01-13 16:25:48 +0500

    com final

--- rabtSpring/src/main/java/rabt/model/Profile.java
+++ rabtSpring/src/main/java/rabt/model/Profile.java
@@ -1,9 +1,11 @@
 package rabt.model;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Profile {
-	private User u_id;
+	//private User u_id; implement after testing
+	private int u_id;
 	private String gender;
 	private String email;
 	private String school;
@@ -34,6 +36,11 @@ public class Profile {
 	public String getAboutme() { return aboutme; }
 	public void setAboutme(String aboutme) {
 		this.aboutme = aboutme;
+	}
+	
+	public Set<Tags> getTags() { return tags; }
+	public void setTags(Set<Tags> tags) {
+		this.tags = tags;
 	}
 	
 }
--- rabtSpring/src/main/java/rabt/model/Tags.java
+++ rabtSpring/src/main/java/rabt/model/Tags.java
@@ -8,4 +8,20 @@ public class Tags {
 	private String name;
 	//private Set<Profile> profile = new HashSet<>(0);
 	private Set<Profile> profile;
+	
+	public int getId() { return this.id; }
+	public void setId(int id ) {
+		this.id = id;
+	}
+	
+	public String getName() { return name; }
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Set<Profile> getProfile() { return profile; }
+	public void setProfile(Set<Profile> profile) {
+		this.profile = profile;
+	}
+	
 }
